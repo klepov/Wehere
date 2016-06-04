7f94a8be9dd598d4900a759f7237643919202135
dima <klepov777@gmail.com>
2016-06-04 21:43:42 +0300

    prepare edit

--- app/src/main/java/klep/wehere/DbHelper.java
+++ app/src/main/java/klep/wehere/DbHelper.java
@@ -33,7 +33,6 @@ public class DbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
         cupboard().withDatabase(db).upgradeTables();
     }
 }
--- app/src/main/java/klep/wehere/common/RegFragment.java
+++ app/src/main/java/klep/wehere/common/RegFragment.java
@@ -13,15 +13,15 @@ import android.text.TextUtils;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.EditText;
 
+import com.bumptech.glide.Glide;
 import com.hannesdorfmann.mosby.mvp.viewstate.ViewState;
-import com.hkm.ui.processbutton.iml.ActionProcessButton;
 import com.mikepenz.community_material_typeface_library.CommunityMaterial;
 import com.mikepenz.iconics.IconicsDrawable;
 import com.sangcomz.fishbun.FishBun;
 import com.sangcomz.fishbun.define.Define;
-
 
 import java.util.ArrayList;
 
@@ -31,14 +31,15 @@ import de.hdodenhof.circleimageview.CircleImageView;
 import klep.wehere.R;
 import klep.wehere.model.RegistrationCredentials;
 import klep.wehere.model.image.CreateImage;
+import klep.wehere.model.users.Data;
+import klep.wehere.utils.Const;
 import klep.wehere.utils.ErrorCode;
 
 
 /**
  * Created by klep.io on 14.02.16.
  */
-public abstract class RegFragment extends BaseViewStateFragment<RegView,RegPresenter> implements RegView, View.OnClickListener {
-
+public abstract class RegFragment extends BaseViewStateFragment<RegView, RegPresenter> implements RegView, View.OnClickListener {
     @Bind(R.id.loginReg)
     EditText loginEdit;
     @Bind(R.id.password1)
@@ -48,98 +49,90 @@ public abstract class RegFragment extends BaseViewStateFragment<RegView,RegPrese
     @Bind(R.id.nameReg)
     EditText nameRegEdit;
     @Bind(R.id.btn_Reg)
-    ActionProcessButton btnReg;
+    Button btnReg;
     @Bind(R.id.btn_photo_parent)
     CircleImageView photoParentButton;
-
     @Bind(R.id.toolbar_reg)
     Toolbar toolbar;
-
-
     public RegOk regOk;
     String path;
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         toolbar.setTitle("назад");
-
         Drawable exitNews = new IconicsDrawable(getActivity())
                 .icon(CommunityMaterial.Icon.cmd_close)
                 .color(ContextCompat.getColor(getActivity(), R.color.md_white_1000))
                 .sizeDp(16);
-
         toolbar.setNavigationIcon(exitNews);
         toolbar.setNavigationOnClickListener(this);
-
-        btnReg.setMode(ActionProcessButton.Mode.ENDLESS);
-        btnReg.setOnClickNormalState(v -> onRegClicked());
-
-        if (savedInstanceState != null){
+        btnReg.setOnClickListener(v -> onRegClicked());
+        if (savedInstanceState != null) {
             path = savedInstanceState.getString("path");
             presenter.resizeImage(path);
         }
-
+        if (getArguments().getParcelable(Const.ID_CHANGE) != null) {
+            Data data = getArguments().getParcelable(Const.ID_CHANGE);
+            loginEdit.setVisibility(View.GONE);
+            password1Edit.setVisibility(View.GONE);
+            password2Edit.setVisibility(View.GONE);
+            nameRegEdit.setText(data.name);
+            Glide.with(this)
+                    .load(Const.IMAGE_URL + data.linkToImage)
+                    .into(photoParentButton);
+            btnReg.setText("Редактировать");
+        } else {
+            btnReg.setText("Регистрация");
+        }
     }
-
-
 
     @Override
     protected int getLayoutRes() {
         return R.layout.fragment_reg;
     }
 
-
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        regOk = (RegOk)activity;
+        regOk = (RegOk) activity;
     }
 
-    @OnClick(R.id.btn_Reg)public void onRegClicked(){
+    @OnClick(R.id.btn_Reg)
+    public void onRegClicked() {
         String login = loginEdit.getText().toString();
         String password1 = password1Edit.getText().toString();
         String password2 = password2Edit.getText().toString();
         String nameReg = nameRegEdit.getText().toString();
-
-        if (TextUtils.isEmpty(login)){
+        if (TextUtils.isEmpty(login)) {
             loginEdit.clearAnimation();
-            Animation shake = AnimationUtils.loadAnimation(getActivity(),R.anim.shake);
+            Animation shake = AnimationUtils.loadAnimation(getActivity(), R.anim.shake);
             loginEdit.startAnimation(shake);
             return;
-        }
-        else if (TextUtils.isEmpty(password1)){
+        } else if (TextUtils.isEmpty(password1)) {
             password1Edit.clearAnimation();
-            Animation shake = AnimationUtils.loadAnimation(getActivity(),R.anim.shake);
+            Animation shake = AnimationUtils.loadAnimation(getActivity(), R.anim.shake);
             password1Edit.startAnimation(shake);
             return;
-        }
-
-        else if (TextUtils.isEmpty(password2)){
+        } else if (TextUtils.isEmpty(password2)) {
             password2Edit.clearAnimation();
-            Animation shake = AnimationUtils.loadAnimation(getActivity(),R.anim.shake);
+            Animation shake = AnimationUtils.loadAnimation(getActivity(), R.anim.shake);
             password2Edit.startAnimation(shake);
             return;
-        }
-
-        else if (TextUtils.isEmpty(nameReg)){
+        } else if (TextUtils.isEmpty(nameReg)) {
             nameRegEdit.clearAnimation();
-            Animation shake = AnimationUtils.loadAnimation(getActivity(),R.anim.shake);
+            Animation shake = AnimationUtils.loadAnimation(getActivity(), R.anim.shake);
             nameRegEdit.startAnimation(shake);
             return;
-        }
-
-        else if (!password1.equals(password2)){
+        } else if (!password1.equals(password2)) {
             showRegError(3);
             return;
         }
-
-
         presenter.doReg(new RegistrationCredentials(login,
                 password1,
                 password2,
                 nameReg,
                 CreateImage.makeImage(path)));
-
     }
 
     @Override
@@ -151,10 +144,11 @@ public abstract class RegFragment extends BaseViewStateFragment<RegView,RegPrese
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        if (path != null) outState.putString("path",path);
+        if (path != null) outState.putString("path", path);
     }
 
-    @OnClick(R.id.btn_photo_parent)public void ChangePhoto(){
+    @OnClick(R.id.btn_photo_parent)
+    public void ChangePhoto() {
         FishBun.with(RegFragment.this)
                 .setAlbumThumnaliSize(150)//you can resize album thumnail size
                 .setPickerCount(1)//you can restrict photo count
@@ -164,12 +158,10 @@ public abstract class RegFragment extends BaseViewStateFragment<RegView,RegPrese
 
     }
 
-
-
     @Override
-    public  ViewState createViewState(){
+    public ViewState createViewState() {
         return new RegViewState();
-    };
+    }
 
     @Override
     public void onNewViewStateInstance() {
@@ -183,33 +175,32 @@ public abstract class RegFragment extends BaseViewStateFragment<RegView,RegPrese
     public void showRegForm() {
         RegViewState vs = (RegViewState) viewState;
         vs.setStateShowRegForm();
-
         setFormEnabled(true);
-
-        btnReg.setProgress(0);
     }
 
-    private void setFormEnabled(boolean enabled){
+    private void setFormEnabled(boolean enabled) {
         loginEdit.setEnabled(enabled);
         password1Edit.setEnabled(enabled);
         password2Edit.setEnabled(enabled);
     }
+
     @Override
     public void showRegError(int error) {
         RegViewState vs = (RegViewState) viewState;
         vs.setStateShowError();
 
-        Snackbar.make(getView(), ErrorCode.getCodeError(getActivity(),error),Snackbar.LENGTH_LONG)
+        Snackbar.make(getView(), ErrorCode.getCodeError(getActivity(), error), Snackbar.LENGTH_LONG)
                 .show();
     }
 
     @Override
-    public void showRegComplete(){
+    public void showRegComplete() {
         regOk.reg();
     }
 
     @Override
-    public void showRegLoading(){};
+    public void showRegLoading() {
+    }
 
     @Override
     public void showImage(Bitmap bitmap) {
@@ -221,14 +212,14 @@ public abstract class RegFragment extends BaseViewStateFragment<RegView,RegPrese
         getActivity().finish();
     }
 
-    public interface RegOk{
+    public interface RegOk {
         void reg();
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode){
+        switch (requestCode) {
             case Define.ALBUM_REQUEST_CODE:
                 try {
                     ArrayList<String> pathFromResult = data.
@@ -236,10 +227,9 @@ public abstract class RegFragment extends BaseViewStateFragment<RegView,RegPrese
                     path = pathFromResult.get(0);
                     presenter.resizeImage(path);
 
-                }catch (NullPointerException ignored){
+                } catch (NullPointerException ignored) {
 
                 }
-
                 break;
         }
     }
