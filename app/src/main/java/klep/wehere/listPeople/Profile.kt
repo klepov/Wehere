c5521690e1996db4a11de1d5b4a3a89ba239811f
dima <klepov777@gmail.com>
2016-06-04 18:48:55 +0300

    Add image profile

--- app/src/main/java/klep/wehere/listPeople/Profile.kt
+++ app/src/main/java/klep/wehere/listPeople/Profile.kt
@@ -2,8 +2,15 @@ package klep.wehere.listPeople
 
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
+import com.mikepenz.fastadapter.adapters.FastItemAdapter
+import com.mikepenz.fastadapter.items.AbstractItem
+import com.mikepenz.fastadapter.utils.ViewHolderFactory
 import com.orhanobut.hawk.Hawk
 import klep.wehere.DbHelper
 import klep.wehere.R
@@ -20,10 +27,13 @@ import nl.qbusict.cupboard.CupboardFactory.cupboard
 class ProfileFragment : BaseFragment() {
     var db: SQLiteDatabase? = null
 
+    private var adapter: FastItemAdapter<UserItem>? = null
+    private var recycler: RecyclerView? = null
+
+
     override fun getLayoutRes(): Int {
         return R.layout.fragment_profile
     }
-
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -32,12 +42,55 @@ class ProfileFragment : BaseFragment() {
         Glide.with(this).load(Const.IMAGE_URL + data.linkToImage).into(image_user)
         val dbHelper = DbHelper(context)
         db = dbHelper.getWritableDatabase()
-        var cursor = cupboard()
+        val cursor = cupboard()
                 .withDatabase(db)
                 .query(Data::class.java)
                 .getCursor()
         user_child_count.text = "${cursor.count} членов семьи"
+        val recycler = recycler_user
+        val layoutManager = GridLayoutManager(context, 3)
+        recycler.layoutManager = layoutManager
 
+        adapter = FastItemAdapter<UserItem>().apply {
+            withOnClickListener({ view, iAdapter, item, i -> false })
+            recycler.adapter = this
+        }
+        val allah = cupboard()
+                .withCursor(cursor)
+                .iterate(Data::class.java)
+        try {
+            adapter?.set(allah.map { UserItem(it) })
+        } finally {
+            cursor.close()
+        }
+    }
+}
+
+class UserItem(val data: Data) : AbstractItem<UserItem, UserHolder>() {
+    companion object {
+        @JvmStatic private val itemFactory = UserItemFactory()
     }
 
+    override fun getLayoutRes() = R.layout.card_user_profile
+
+    override fun getType() = R.id.user_list_item
+
+    override fun getFactory() = itemFactory
+
+    override fun bindView(holder: UserHolder) {
+        super.bindView(holder)
+        holder.name.text = data.name
+        Glide.with(holder.image.context)
+                .load(Const.IMAGE_URL + data.linkToImage)
+                .into(holder.image)
+    }
+}
+
+class UserItemFactory : ViewHolderFactory<UserHolder> {
+    override fun create(v: View) = UserHolder(v)
+}
+
+class UserHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val image = itemView.findViewById(R.id.family_user_image) as ImageView
+    val name = itemView.findViewById(R.id.family_user) as TextView
 }
